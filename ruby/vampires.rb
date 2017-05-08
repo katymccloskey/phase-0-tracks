5179dd3f7441ddfe0425e5aaf86a5425a52111cd
Katy McCloskey <you@example.com>
2017-05-08 16:12:02 -0500

    add vampire interview questions

---
+++ ruby/vampires.rb
@@ -0,0 +1,14 @@
+puts "What is your name?"
+name = get.chomps
+
+puts "How old are you?"
+age = get.chomps
+
+puts "What year were you born?"
+birth_year = get.chomps
+
+puts "Our cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = get.chomps
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = get.chomps
