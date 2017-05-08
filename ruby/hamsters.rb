1d671fcff1b0c991a91d61a6214330f9d684a357
Katy McCloskey <you@example.com>
2017-05-08 12:47:52 -0500

    add pseudocode

---
+++ ruby/hamsters.rb
@@ -0,0 +1,22 @@
+# ask user hamster's name
+  #name= gets.chomp
+
+# ask user hamster's volume level on scale of 1-10
+  #volume= gets.chomp
+  #volume= true if <=10, false if >10
+
+# ask user hamster's fur color
+  # fur_color= gets.chomp
+
+#while good_candidate!=true
+# ask user if hamster is a good candidate for adoption y/n
+  # if input == "y"
+    # good_candidate= true
+  # elsif input =="n"
+    # good_candidate= true
+  # else puts "I didn't understand"
+#end
+
+#ask user hamster's estimated age
+  # age=gets.chomp
+  # if age is blank, age=nil
