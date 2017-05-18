257fb2bb8706c4140e74c9c2d9f5d8911011d2ab
Katy McCloskey <you@example.com>
2017-05-17 19:20:49 -0500

    forgot to switch branches

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,8 +4,8 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+ => “InVeStIgAtIoN”
 
 # "zom".<???>
 # => “zoom”
@@ -13,8 +13,8 @@
 # "enhance".<???>
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase
+ => "STOP! YOU’RE UNDER ARREST!"
 
 # "the usual".<???>
 #=> "the usual suspects"
@@ -32,7 +32,7 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
 # "How many times does the letter 'a' appear in this string?".<???>
