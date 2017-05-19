39daaa3a2d0da4e4afaaf9d34283d9812cd2f638
Katy McCloskey <you@example.com>
2017-05-19 17:33:11 -0500

    add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,15 @@
+design_info = {}
+
+#tell designer we will collect information
+puts "Welcome! We'll be inputting your client info."
+#ask for the clients:
+puts "How old is your client?"
+    # name
+    # age
+    # number of children
+    # decor theme
+#ask designer if they need to update a key
+  # if they say none, skip it
+  # if they enter a key (like decor_theme), ask for a new value and udpate the :decor_theme key
+
+#print the hash and exit the program
