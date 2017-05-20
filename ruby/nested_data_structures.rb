e245414c3042bcb83afc450b5ef6d215b59cf828
Katy McCloskey <you@example.com>
2017-05-20 12:45:28 -0500

    add classroom hash and first nested structure of desk 1

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,35 @@
+classroom = {
+  desk_1: {
+    student_info: {
+      student_name: "Katy",
+      student_age: "31"
+      },
+
+    supplies: [
+      "pencil",
+      "marker",
+      "paper"
+      ]
+  }
+}
+  desk_2: {
+    student_info: {
+      student_name: "Krista"
+      student_age: "29"
+    },
+
+    supplies: [
+      ]
+
+  }
+
+  desk_3: {
+
+  }
+
+
+
+
+
+
+}
