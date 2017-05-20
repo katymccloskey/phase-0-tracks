0ddcdb357ea390cf91ac9db075ae0eb2b71a0b19
Katy McCloskey <you@example.com>
2017-05-20 12:53:07 -0500

    add nested data for desk 2 and desk 3

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -10,26 +10,30 @@ classroom = {
       "marker",
       "paper"
       ]
-  }
-}
+  },
   desk_2: {
     student_info: {
-      student_name: "Krista"
+      student_name: "Krista",
       student_age: "29"
     },
 
     supplies: [
+      "scissors",
+      "glue",
+      "play-dough"
       ]
-
-  }
+  },
 
   desk_3: {
+    student_info: {
+      student_name: "Regina",
+      student_age: "28"
+    },
 
+    supplies: [
+      "crayons",
+      "manipulatives",
+      "books"
+      ]
   }
-
-
-
-
-
-
 }
