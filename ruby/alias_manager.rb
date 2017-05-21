2812258b0298258e1441d07e4513ca9dee6782b1
Katy McCloskey <you@example.com>
2017-05-21 15:43:36 -0500

    save for later

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -8,17 +8,17 @@ def alias_method
   until name == "quit"
   puts "what is your name? type quit to exit"
   alias_name = {
-    :name => name_2
+    :name_1 => name_2
   }
   name = gets.chomp
     if name != "quit"
-      alias_name[:name] = name
+      alias_name[:name_1] = name
       name_2 = name.split(" ").reverse.join(" ").tr('aeiou', 'eioua').tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')
     break if name == "quit"
     end
   end
-  alias_name.each do
-    p "#{name} is actually #{name_2}!"
+  alias_name.each do |name_1, name_2|
+    p "#{name_1} is actually #{name_2}!"
   end
 end
 
