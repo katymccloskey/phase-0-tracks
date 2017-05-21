2e3f83ea4bf56adcca5d556ea8058f13340b20b9
Katy McCloskey <you@example.com>
2017-05-21 13:27:31 -0500

    fix method to print alias name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -4,16 +4,22 @@
 
 def alias_method
   name = ""
+  name_2 = ""
   until name == "quit"
   puts "what is your name? type quit to exit"
+  alias_name = {
+    :name => name_2
+  }
   name = gets.chomp
     if name != "quit"
-      str = name
-      p str.split(" ").reverse.join(" ").tr('aeiou', 'eioua').tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')
+      alias_name[:name] = name
+      name_2 = name.split(" ").reverse.join(" ").tr('aeiou', 'eioua').tr('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyzb')
     break if name == "quit"
     end
   end
+  alias_name.each do
+    p "#{name} is actually #{name_2}!"
+  end
 end
-
 
 alias_method
