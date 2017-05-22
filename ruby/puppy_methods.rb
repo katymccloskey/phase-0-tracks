e3c3ac57f836f34d5223c669207e933d2276248d
Katy McCloskey <you@example.com>
2017-05-22 16:23:25 -0500

    add play dead and dog years method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -16,6 +16,15 @@ class Puppy
   def roll_over
     puts "*rolls over*"
   end
+
+  def dog_years(n)
+    puts n * 7
+  end
+
+  def play_dead
+    puts "lays on back quietly"
+  end
+
 end
 
 Dash = Puppy.new
@@ -24,4 +33,8 @@ Dash.fetch("ball")
 Dash.speak(5)
 
 Dash.roll_over
+
+Dash.dog_years(7)
+
+Dash.play_dead
 
