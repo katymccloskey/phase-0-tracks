15bef66e2f8005fb17718e5fcff763042a72293b
Katy McCloskey <you@example.com>
2017-05-24 19:25:27 -0500

    add celebrate birthday and get mad at method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,21 +16,43 @@ class Santa
     puts "That was a good #{type}!"
   end
 
-end
-
-# santa1 = Santa.new
-
-# santa1.speak
-
-# santa1.eat_milk_and_cookies("snickerdoodle")
-
-santas = []
-genders = ["agender", "female", "male"]
-ethnicities = ["latina", "black", "phillipino"]
-
-genders.length.times do |i|
-  santas << Santa.new(genders[i], ethnicities[i])
+  def celebrate_birthday
+    @age = @age + 1 #age Santa by one year
+    p "#{@age}"
   end
 
-p santas
+  def get_mad_at(name)
+    @reindeer_ranking.insert(8, @reindeer_ranking.delete_at(@reindeer_ranking.index(name)))
+  end
 
+  def gender= (new_gender)
+    @gender = new_gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+end
+
+santa1 = Santa.new("female", "latina")
+
+p santa1.get_mad_at("Vixen")
+
+#  santa1.speak
+
+# # santa1.eat_milk_and_cookies("snickerdoodle")
+
+# santas = []
+# genders = ["agender", "female", "male"]
+# ethnicities = ["latina", "black", "phillipino"]
+
+# genders.length.times do |i|
+#   santas << Santa.new(genders[i], ethnicities[i])
+#   end
+
+# p santas
