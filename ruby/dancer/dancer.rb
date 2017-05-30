98a873e5cdc602827ed88d6cdd2adaaf2efe29e0
Katy McCloskey <you@example.com>
2017-05-29 23:04:51 -0500

    add method begin next dance

--- ruby/dancer/dancer.rb
+++ ruby/dancer/dancer.rb
@@ -25,8 +25,19 @@ class Dancer
   end
 
   def begin_next_dance
-
+    while card.length > 0
+      card.each do |name|
+        card.delete name
+        p "Now dancing with #{name}."
+        p card
+      end
+    end
   end
-
+  #So, when I test this method, the return value of the rspec test that should be "Now dancing with Mikhail Baryshnikov" is returning "nil." When I test the code in the repl, it seems to perform as it should- it adds the names to the array, then goes through the names and says "Now dancing with (first dance)", then prints the updated array with the first dance name taken off, then says "Now dancing with (second dance)", and then returns the empty array, as it has iterated through and removed the names after. I think I'm having a problem with what is being returned. Will keep looking at it but want to make sure I don't miss the submission deadline! This note is mostly so that when we review the code later I can make sure and ask about return values if I haven't figured it out by then :)
 end
 
+dancer = Dancer.new("Misty Copeland", 33, "pink")
+
+p dancer.queue_dance_with("Mikhail Baryshnikov")
+p dancer.queue_dance_with("Anna Pavlova")
+dancer.begin_next_dance
