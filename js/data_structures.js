7864013c3c4d77fcd1f8ea6ed675b0d86660c426
Katy McCloskey <you@example.com>
2017-06-03 11:37:03 -0500

    add horses object

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,19 @@ var names = ["Leslie", "Ron Swanson", "Andy", "Jean-Ralphio"];
 colors.push("surprise color");
 
 names.push("Lil Sebastian");
+
+console.log(colors)
+console.log(names)
+
+function createHorses (names, colors)
+{
+  var horses = {};
+  if (names.length != colors.length)
+    return null;
+  for (var index in names)
+    horses [names[index]] =  colors[index];
+  return horses
+};
+
+var horses = createHorses(names, colors);
+console.log(horses)
