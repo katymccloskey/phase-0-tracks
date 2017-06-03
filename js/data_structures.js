68ebee4030d32f50d4a33433b14d8271ac6d17b1
Katy McCloskey <you@example.com>
2017-06-03 10:54:43 -0500

    add arrays of horse colors and names

---
+++ js/data_structures.js
@@ -0,0 +1,7 @@
+var colors = ["cerulean", "sparkle", "rainbow", "glitter"];
+
+var names = ["Leslie", "Ron Swanson", "Andy", "Jean-Ralphio"];
+
+colors.push("surprise color");
+
+names.push("Lil Sebastian");
