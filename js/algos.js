5571463fa164d9707be10b399494b06539637dd6
Katy McCloskey <you@example.com>
2017-06-04 13:05:18 -0500

    add pseudocode for release 1 and 2

--- js/algos.js
+++ js/algos.js
@@ -5,22 +5,46 @@
 // compare length of each item, storing current longest in variable1. when longest is found, store in var2
 // return longest item
 
-function findLongest(array) {
-var longest = 0;
-var longestWord = "";
-
-for(var i=0; i < array.length; i++) {
-    if (array[i].length > longest) {
-        longest = array[i].length;
-        longestWord = array[i]
-      }
+function findLongestWord(array) {
+  var char_count = 0;
+  var longestWord = "";
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length > char_count) {
+      char_count = array[i].length;
+      longestWord = array[i];
     }
+  }
   return longestWord;
 }
+
+var array = ["cat", "horse", "elephant"]
+console.log(findLongestWord(array))
+
 var array = ["cat", "moth", "unicorn"];
-console.log(findLongest(array));
+console.log(findLongestWord(array));
 
 var array = ["OMG this was challenging", "but I did it!", "finally"];
-console.log(findLongest(array));
+console.log(findLongestWord(array));
 
 
+//write a function that takes two objects (hashes) anc checks to see if they share at least one key-value pair
+function anyMatches(hash1, hash2) {
+  for (var i = 0, i < hash1.length, i++)
+}
+//if they share at least one key value pair, return as true
+// if they do not share a key value pair, return as false
+// function should take 2 objects as argument
+// check to see if any keys match. if not, return false
+// if any keys match, check those keys to see if their values match
+// if not, return false
+// if at least one key value pair matches, return true
+
+
+// write a function that takes an integer for length, and returns an array of strings of the given length (so if we ran function with argument of 3, we'd get three random strings/words of letters back). Words should be of varying length from 1-10.
+  //function(n) n is integer for length
+    //while loop count is < n,
+    // make word of random letters a-z
+      // that is random length 1-10
+
+
+// add driver code that does the following 10 times: generates array, prints array,feeds array through findLongestWord function, and prints result
